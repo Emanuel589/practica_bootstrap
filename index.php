4a9a9a9a3a6978081fa809513aa50cacaa5f9e28
Emanuel <ebaltazar@udabol.edu.bo>
2016-11-24 11:13:28 -0400

    actualizando estructura delproyecto

---
+++ index.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+<html>
+  <head lang="es">
+    <meta charset="UTF-8">
+  	<title>Pgina con Bootstrap 3</title>
+  	<!-- Latest compiled and minified CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" >
+
+  </head>
+  <body>
+  	 <h1>Estilo Bootstrap</h1>
+     <p>Hola Mundo</p> 
+
+     <!-- Latest compiled and minified JavaScript -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+  	<!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
