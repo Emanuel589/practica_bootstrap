60e7e986666ba75e82ce48ff1d50131d1afd851f
Emanuel <ebaltazar@udabol.edu.bo>
2016-12-01 18:44:28 -0400

    poniendo estilos en detalle boton registro probando mapa de google maps

--- detail.php
+++ detail.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
   	<title>Pagina con Bootstrap 3</title>
     <?php include 'inc/head_common.php';?>
-    <link rel="stylesheet" href="css/datail.css">
+    <link rel="stylesheet" href="css/detail.css">
   </head>
   <body>  	 
      <?php include 'inc/header.php';?>
@@ -53,7 +53,16 @@
             <section id="registration">
                 <h3>Registro</h3>
                 <p>Hay varios pre-procesadores disponibles para nosotros, siendo los más comunes Sass (con SCSS) y LESS. En este artículo, como ya lo habrás adivinado, hablaremos de Sass que es el que acostumbro a usar.</p>
-                <div id="registration-badge"></div>
+                <div id="registration-badge">
+                  <h3 class="price">99 $</h3>
+                  <p class="included">
+                     workshop + <br>
+                     desayuno + <br>
+                     almuerzo
+                  </p>
+                  <div class="clearfix"></div>
+                  <a href="#" class="do-register">ME APUNTO!</a> 
+                </div>
             </section>
             <section id="agenda">
                 <h3>Agenda del día</h3>
@@ -120,6 +129,7 @@
        </section>
        <section id="location-info">
        <!-- Mapa de Google-->
+          <div class="map-canvas"></div>
           <div class="container">
             <div class="row">
                <div class="col-xs-5">
@@ -176,5 +186,18 @@
      </article>
     <?php include 'inc/footer.php';?>
     <?php include 'inc/footer_common.php';?>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+    <script>
+        var map;
+        function initialize {
+          var mapOptions = {
+            zoom: 8,
+            scrollwheel: false,
+            center: new google.maps.LatLng(-34.397,150.644)
+          };
+          map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+        }
+        google.maps.event.addDomListener(window,'load',initialize);
+    </script>
   </body>
 </html>
