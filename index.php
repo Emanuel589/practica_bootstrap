49b99ea873ccf5a0282e04c0e773c4cfb363b9d0
Emanuel <ebaltazar@udabol.edu.bo>
2016-12-05 18:34:56 -0400

    adaptacion reponsive zona workshop

--- index.php
+++ index.php
@@ -14,29 +14,29 @@
      	<article id="w1" class="workshop workshop-left">
      	<div class="container">
      		<div class="row">
-     			<div class="col-xs-12 col-sm-6 main-info">
-     				<h2>Desarrolla front-end con Bootstrap 3</h2>
-     				<p>Este parrafo es de prueba para la pagina. Verificando la maqueación.</p>
-     				<a href="detail.php" class="btn btn-custom">Me Apunto</a>
-     				<a href="detail.php" class="more-info-linf">Quiero mas Información</a>
-     			</div>
-     			<div class="col-xs-12 col-sm-4 col-sm-offset-m date-info">
+     		    <div class="col-xs-12 col-sm-3 col-sm-push-9 col-md-4 col-md-push-8 col-lg-6 col-lg-push-6 date-info">
      				<h3 class="highlinghted"><strong>W21 </strong>13/14 de Abril</h3>
      				<h3 class="highlinghted">20 Plazas</h3>
      				<h3 class="highlinghted">99 Bs</h3>
      			</div>
+     			<div class="col-xs-12 col-sm-9 col-sm-pull-3 col-md-8 col-md-pull-4 col-lg-6 col-lg-pull-6 main-info">
+     				<h2>Desarrolla front-end con Bootstrap 3</h2>
+     				<p>Este parrafo es de prueba para la pagina. Verificando la maqueación.</p>
+     				<a href="detail.php" class="btn btn-custom">Me Apunto</a>
+     				<a href="detail.php" class="more-info-linf">Quiero mas Información</a>
+     			</div>     			
      		</div>
      	</div>	
      	</article>
      	 <article id="w2" class="workshop workshop-right">
      	 <div class="container">
      	 	<div class="row">
-     			<div class="col-xs-12 col-sm-4 date-info">
+     			<div class="col-xs-12 col-sm-3 col-md-4 col-lg-6 date-info">
      				<h3 class="highlinghted"><strong>W21 </strong>10/14 de Abril</h3>
      				<h3 class="highlinghted">50 Plazas</h3>
      				<h3 class="highlinghted">120 Bs</h3>
      			</div>
-     			<div class="col-xs-12 col-sm-6 col-sm-offset-2 main-info">
+     			<div class="col-xs-12 col-sm-9 col-md-8 col-lg-6 main-info">
      				<h2>Desarrolla front-end con Bootstrap 3</h2>
      				<p>Este parrafo es de prueba para la pagina. Verificando la maqueación.</p>
      				<a href="detail.php" class="btn btn-custom">Me Apunto</a>
