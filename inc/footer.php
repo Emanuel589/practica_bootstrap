b7fabae19a06a83de9308122d3103020d5c2dd01
Emanuel <ebaltazar@udabol.edu.bo>
2016-12-05 18:19:23 -0400

    footer responsive

--- inc/footer.php
+++ inc/footer.php
@@ -1,31 +1,31 @@
 <footer>
 <div class="container">
 	<div class="row">
-		<div class="col-xs-5">
+		<div class="col-xs-12 col-sm-5">
 			<h3>PRÓXIMOS WORKSHOPS</h3>
 			<ul class="next-workshops">
 				<li><a href="detail.php">Título del primer Workshop de prueba</a></li>
                 <li><a href="detail.php">Título del segundo Workshop de prueba</a></li>
 			</ul>
 		</div>
-        <div class="col-xs-6 col-xs-offset-1">
+        <div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
         	<h3>NO TE PIERDAS NUESTRAS NOVEDADES</h3>
         	<form id="subscribe-form" action="">
         		<div class="row">
-        			<div class="col-xs-8">
+        			<div class="col-xs-7 col-sm-8">
         				<div class="form-group">
         					<label class="sr-only" for="email">Tu correo Electrónico</label>
         					<input id="email" name="email" type="text" class="form-control input-lg" placeholder="Tu correo electrónico">
         				</div>
         			</div>
-        			<div class="col-xs-4">
+        			<div class="col-xs-5 col-sm-4">
         				<button class="btn btn-custom btn-lg">SUSCRIBIME</button>		
         			</div>
         		</div>
         	</form>
         </div> 
         <div class="clearfix"></div>
-        <div class="col-xs-5">
+        <div class="col-xs-12 col-sm-5">
 			<h3>SOBRE LOS WORKSHOPS</h3>
 			<p>Millions of amazing sites across the web
 			 are being built with Bootstrap. Get started
@@ -33,7 +33,7 @@
 			  by exploring some of our favorites.</p>
 			  
 		</div>
-        <div class="col-xs-6 col-xs-offset-1">
+        <div class="col-xs-12 col-sm-7 col-md-6 col-md-offset-1">
         	<h3>¿Te gusta? Corre la voz</h3>
         	<p>Modificado - Millions of amazing sites across the web
 			 are being built with Bootstrap. Get started
--- index.php
+++ index.php
@@ -12,34 +12,38 @@
      <?php include 'inc/header.php';?>
      <div class="workshop-list">
      	<article id="w1" class="workshop workshop-left">
+     	<div class="container">
      		<div class="row">
-     			<div class="col-xs-8 main-info">
+     			<div class="col-xs-12 col-sm-6 main-info">
      				<h2>Desarrolla front-end con Bootstrap 3</h2>
      				<p>Este parrafo es de prueba para la pagina. Verificando la maqueación.</p>
      				<a href="detail.php" class="btn btn-custom">Me Apunto</a>
      				<a href="detail.php" class="more-info-linf">Quiero mas Información</a>
      			</div>
-     			<div class="col-xs-3 col-xs-offset-1 date-info">
+     			<div class="col-xs-12 col-sm-4 col-sm-offset-m date-info">
      				<h3 class="highlinghted"><strong>W21 </strong>13/14 de Abril</h3>
      				<h3 class="highlinghted">20 Plazas</h3>
      				<h3 class="highlinghted">99 Bs</h3>
      			</div>
      		</div>
+     	</div>	
      	</article>
      	 <article id="w2" class="workshop workshop-right">
-     		<div class="row">
-     			<div class="col-xs-3 date-info">
+     	 <div class="container">
+     	 	<div class="row">
+     			<div class="col-xs-12 col-sm-4 date-info">
      				<h3 class="highlinghted"><strong>W21 </strong>10/14 de Abril</h3>
      				<h3 class="highlinghted">50 Plazas</h3>
      				<h3 class="highlinghted">120 Bs</h3>
      			</div>
-     			<div class="col-xs-8 col-xs-offset-1 main-info">
+     			<div class="col-xs-12 col-sm-6 col-sm-offset-2 main-info">
      				<h2>Desarrolla front-end con Bootstrap 3</h2>
      				<p>Este parrafo es de prueba para la pagina. Verificando la maqueación.</p>
      				<a href="detail.php" class="btn btn-custom">Me Apunto</a>
      				<a href="detail.php" class="more-info-linf">Quiero mas Información</a>
      			</div>
      		</div>
+     	 </div>   		
      	</article>
      </div>
   	 <?php include 'inc/footer.php'; ?>
